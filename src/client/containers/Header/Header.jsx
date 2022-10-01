e6f62903cc400b71167962e4a5b814d9e4929c59
배명호(goorm) <mac.bae@goorm.io>
2022-10-01 11:53:50 +0000

    build(package): reactstrap 패키지 추가

--- src/client/containers/Header/Header.jsx
+++ src/client/containers/Header/Header.jsx
@@ -1,9 +1,11 @@
+import { Button } from 'reactstrap';
 import Logo from '@/components/Logo';
 import styles from './Header.module.scss';
 
 const Header = () => (
 	<header className={styles.Header}>
 		<Logo showLabel />
+		<Button color="secondary">Get Started</Button>
 	</header>
 );
 
