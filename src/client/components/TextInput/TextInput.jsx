03fd7450adf9afb04b59bdf039aa9d17b6424672
배명호(goorm) <mac.bae@goorm.io>
2022-10-01 13:28:13 +0000

    feat(input): 일기 입력 컴포넌트 동적 높이 스타일 적용 추가

--- src/client/components/TextInput/TextInput.jsx
+++ src/client/components/TextInput/TextInput.jsx
@@ -1,13 +1,35 @@
 import Avatar from 'react-avatar';
-import { Input } from 'reactstrap';
+import { Input, Button } from 'reactstrap';
 
+import ChevronRightIcon from '@/assets/chevron-right.svg';
 import styles from './TextInput.module.scss';
 
 const TextInput = (props) => {
+	const onInput = (e) => {
+		e.target.style.height = '2rem';
+		e.target.style.height = `${e.target.scrollHeight}px`;
+	};
+
 	return (
 		<div className={styles.TextInput}>
-			<Avatar name="Mac" size="56" textSizeRatio="2" color="#343a40" round />
-			<Input type="textarea" className={styles.TextInput__input} {...props} />
+			<Avatar
+				className={styles.TextInput__avatar}
+				name="Mac"
+				size="56"
+				textSizeRatio="2"
+				color="#343a40"
+				round
+			/>
+			<Input
+				type="textarea"
+				n
+				className={styles.TextInput__input}
+				onInput={onInput}
+				{...props}
+			/>
+			<Button className={styles.TextInput__submit} color="link">
+				<img src={ChevronRightIcon} alt="submit" />
+			</Button>
 		</div>
 	);
 };
