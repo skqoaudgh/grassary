ab97893e49b9fb272d1e11e560783a8edda63614
배명호(goorm) <mac.bae@goorm.io>
2022-10-01 04:06:37 +0000

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'airbnb',
+		'plugin:prettier/recommended',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', 'plugin:react/jsx-runtime'],
+	rules: {
+		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+	},
+};
